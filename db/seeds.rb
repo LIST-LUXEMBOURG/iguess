4a5adf23f52edc3baec50d0a5875db559b9304a7
Christopher Eykamp <christopher.eykamp@tudor.lu>
2012-10-22 11:09:13 +0200

    Update SRS for Aberdeen

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 
-[['Aberdeen',    '', 12, 'EPSG:900913', -243400, -227700, 7782600, 7801700],
+[['Aberdeen',    '', 12, 'EPSG:27700', -243400, -227700, 7782600, 7801700],
  ['Gent',        '', 12, 'EPSG:31370',  407000,  423200, 6622900, 6644600],
  ['Ludwigsburg', 'http://logis.ludwigsburg.de/mapguide2011/mapagent/mapagent.fcgi', 13, 'EPSG:31467', 3495664, 3536658, 5406951, 5429311],
  ['Montreuil',   '', 13, 'EPSG:2154',  268000,  276000, 6249000, 6254000],
