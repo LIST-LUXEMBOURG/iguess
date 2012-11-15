28bf703dbf3cc06cfa10fd554c868287085f7ee5
Christopher Eykamp <christopher.eykamp@tudor.lu>
2012-11-15 19:09:28 +0100

    Formatting

--- app/assets/javascripts/webgis/MapInit.js
+++ app/assets/javascripts/webgis/MapInit.js
@@ -30,11 +30,11 @@
     var boundsMap  = new OpenLayers.Bounds(-1015000, 5845000, 1100000, 8000000);  
     
     WebGIS.map = new OpenLayers.Map("BroadMap",{
-      projection: new OpenLayers.Projection(WebGIS.mapProjection),
-      displayProjection: new OpenLayers.Projection(WebGIS.displayProjection),
-      units: "m",
-      maxExtent: boundsMap,
-      controls: []
+        projection:        new OpenLayers.Projection(WebGIS.mapProjection),
+        displayProjection: new OpenLayers.Projection(WebGIS.displayProjection),
+        units:             "m",
+        maxExtent:         boundsMap,
+        controls:          []
     });
 
     var mp = new OpenLayers.Control.MousePosition({
