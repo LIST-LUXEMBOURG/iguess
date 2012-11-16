11c944f00b23c5e8b65156634c66e25cd524d54e
Christopher Eykamp <christopher.eykamp@tudor.lu>
2012-11-16 15:29:01 +0100

    Suppress warnings

--- app/assets/javascripts/webgis/MapInit.js
+++ app/assets/javascripts/webgis/MapInit.js
@@ -60,10 +60,10 @@
             "Google Streets", // the default
             {numZoomLevels: 20}
     );
-    var ghyb = new OpenLayers.Layer.Google(
-            "Google Hybrid",
-            {type: google.maps.MapTypeId.HYBRID, numZoomLevels: 20}
-    );
+    // var ghyb = new OpenLayers.Layer.Google(
+    //         "Google Hybrid",
+    //         {type: google.maps.MapTypeId.HYBRID, numZoomLevels: 20}
+    // );
     var gsat = new OpenLayers.Layer.Google(
             "Google Satellite",
             {type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 22}
