9957097ae10d5a7e561e55cdc68618d9f4249e59
Luís de Sousa <luis.a.de.sousa@gmail.com>
2012-12-19 16:53:12 +0100

    Fixed zoom to City control.

--- app/assets/javascripts/webgis/MapPanels.js
+++ app/assets/javascripts/webgis/MapPanels.js
@@ -124,7 +124,8 @@ WebGIS.createTbarItems = function(map) {
     iconCls: "maxExtent",
     map: map,
     tooltip: "Zoom to max extent",
-    control: new OpenLayers.Control.ZoomToMaxExtent()
+    //control: new OpenLayers.Control.ZoomToMaxExtent()
+    handler: WebGIS.zoomToCity
   }));
   actions.push(new GeoExt.Action({
     iconCls: "zoomin",
