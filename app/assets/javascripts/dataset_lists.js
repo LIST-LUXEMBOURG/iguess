18ce6d1568f4fece386be8b8a6f7d11bddf21a41
Christopher Eykamp <christopher.eykamp@tudor.lu>
2013-01-28 15:48:30 +0100

    More concise

--- app/assets/javascripts/dataset_lists.js
+++ app/assets/javascripts/dataset_lists.js
@@ -30,7 +30,7 @@ var processUrl = function(url)
   // This function will be called for every dataset registered with the current city.  Many will have the same
   // server.  Avoid processing the same server twice.
   // Called from renderTable(), which is called from onCityChange() event handler
-  if(processedUrls[url] === true) {  return;  }
+  if(processedUrls[url]) {  return;  }
 
   processedUrls[url] = true;
 
