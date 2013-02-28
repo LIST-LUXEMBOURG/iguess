103bf3a92910b272e762361d6070140508e75d82
Christopher Eykamp <christopher.eykamp@tudor.lu>
2013-02-28 11:35:35 +0100

    Clean out controller

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -25,26 +25,15 @@ class MapsController < ApplicationController
 
     @datasets = Dataset.all
     @dataserver_urls = @datasets.map{|d| d.server_url}.uniq
-
-    @configs = ModConfig.all
     
     # The following will create a transposed list of all the requirements of all our models.
     # The keys are module_requirements objects, and the values are an array of module_configurations where that 
     # key is used.
-    @requirements_by_config = {}  # Create empty hash
-                                                      
-    # @configs.collect{ |c| c.mod.module_requirements.collect{ |r| @requirements_by_config[r.parameter_type.name] ||= [];        # Create an empty array if needed
-                                                                 # @requirements_by_config[r.parameter_type.name].push(c) } }    # Append config to the array
 
     @datasetsByRequirement = {}
     
-    @datasets.collect{ |d|  @datasetsByRequirement[d.dataset_type] ||= [];     # Create an empty array if needed
-                            @datasetsByRequirement[d.dataset_type].push(d)     # Append dataset to the array
-                     }
-    
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @datasets }
     end
   end
 end
