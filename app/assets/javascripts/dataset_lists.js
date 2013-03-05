3ccabe91b68e3df54d270cb146e157c08dc79d47
Christopher Eykamp <christopher.eykamp@tudor.lu>
2013-03-05 12:24:34 +0100

    Get id into tag container, pass fewer params

--- app/assets/javascripts/dataset_lists.js
+++ app/assets/javascripts/dataset_lists.js
@@ -137,17 +137,17 @@ var updateDatasets = function(serverUrl, dataProxy, records, service)  // servic
 
 
 // Create a popup info display for this dataset 
-var renderInfoTable = function(dataset, datasetId) 
+var renderInfoTable = function(dataset) 
 {
   var serverUrlId = registeredDataAndMapServers[dataset.server_url];
 
-  return '<div class="infotable" id="infotable-' + datasetId + '">' +
+  return '<div class="infotable" id="infotable-' + dataset.id + '">' +
             '<div class="close"></div>' +
-            '<h1><span class="dataset-title-' + datasetId + '"></h1>' +
-            '<div class="dataset-descr-' + datasetId + '"></div>' +
+            '<h1><span class="dataset-title-' + dataset.id + '"></h1>' +
+            '<div class="dataset-descr-' + dataset.id + '"></div>' +
             '<div style="overflow:hidden"><dl>' +
               '<dt>Server Name:</dt><dd class="server-name-' + serverUrlId + '"></dd>' +
-               '<dt>Data Services:</dt><dd id="results-' + datasetId + '">Waiting for response from server...</dd>' + 
+               '<dt>Data Services:</dt><dd id="results-' + dataset.id + '">Waiting for response from server...</dd>' + 
                
             '</dl></div>' +
             '<div style="overflow:hidden" class="technical-details">' +
