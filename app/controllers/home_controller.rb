fcf017878995e8039a334d0a69418b2df72d6827
Luís de Sousa <luis.a.de.sousa@gmail.com>
2013-06-28 17:01:46 +0200

    Trying to get GeoProxy to process POST requests.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,22 +5,54 @@ class HomeController < ApplicationController
     require 'timeout'
 
     rawurl = CGI::unescape(params[:url])
+    
     fixedurl = rawurl.gsub('\\', '%5C')   # Escape backslashes... why oh why???!?
     r = nil;
 
     status = 200
     content_type = 'text/html'
+    #content_type = 'text/xml'
 
     
     timeout_duration = 15      # Give 5 second timeout...  long enough?
 
     begin
       Timeout::timeout(timeout_duration) {
-        r = Net::HTTP.get_response(URI.parse(fixedurl))
-
-        status = r.code    # If there was an  error, pass that code back to our caller
-        @page = r.body
-        content_type = r['content-type']
+        
+        if request.get? then
+        
+          print "GeoProxy sending GET request to: " + String(fixedurl)
+          
+          res = Net::HTTP.get_response(URI.parse(fixedurl))
+  
+          status = res.code    # If there was an  error, pass that code back to our caller
+          @page = res.body
+          content_type = res['content-type']
+        
+        elsif request.post? then
+          
+          print "GeoProxy sending POST request to: " + String(fixedurl)
+          binding.pry
+          
+          url = URI.parse(String(fixedurl))
+          post = Net::HTTP::Post.new(url.path)
+          post.body = request.body
+          res = Net::HTTP.start(url.host, url.port) {|http| http.request(post)}
+                    
+          #res = Net::HTTP.post_form(String(fixedurl), params[:text1])
+          #res.body = request.body 
+          #puts res.body
+          
+          status = res.code    # If there was an  error, pass that code back to our caller
+          @page = res.body
+          content_type = res['content-type']
+          
+        else 
+          
+          print "GeoProxy couldn't decode request: " + String(fixedurl)
+          
+        end
+        
       }
 
     rescue Timeout::Error
