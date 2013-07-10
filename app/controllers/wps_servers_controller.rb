a5e1eaf1d6d4ade9c705d151fab13bdabf41e5c8
Christopher Eykamp <christopher.eykamp@tudor.lu>
2013-07-10 16:04:39 +0200

    Update wps_servers controller to reflect new deletion regime

--- app/controllers/wps_servers_controller.rb
+++ app/controllers/wps_servers_controller.rb
@@ -3,13 +3,14 @@ class WpsServersController < ApplicationController
   # GET /wps_servers.json
   def index
     @current_city  = User.getCurrentCity(current_user, cookies)
-    @wps_servers   = WpsServer.find_all_by_city_id(@current_city.id)
+    @wps_servers   = WpsServer.find_all_by_city_id_and_deleted(@current_city.id, :false)
 
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @wps_servers }
     end
   end
+
 
   # GET /wps_servers/1
   # GET /wps_servers/1.json
@@ -21,6 +22,7 @@ class WpsServersController < ApplicationController
       format.json { render json: @wps_server }
     end
   end
+
 
   # GET /wps_servers/new
   # GET /wps_servers/new.json
@@ -48,6 +50,7 @@ class WpsServersController < ApplicationController
     @wps_server = WpsServer.find(params[:id])
   end
 
+
   # POST /wps_servers
   # POST /wps_servers.json
   def create
@@ -60,7 +63,7 @@ class WpsServersController < ApplicationController
 
     respond_to do |format|
       if @wps_server.save
-        format.html { redirect_to @wps_server, notice: 'Wps server was successfully created.' }
+        format.html { redirect_to @wps_server, notice: "Wps server was successfully created." }
         format.json { render json: @wps_server, status: :created, location: @wps_server }
       else
         format.html { render action: "new" }
@@ -69,19 +72,20 @@ class WpsServersController < ApplicationController
     end
   end
 
+
   # PUT /wps_servers/1
   # PUT /wps_servers/1.json
   def update
-    if not user_signed_in?    # Should always be true
-      showError("Insufficient permissions -- you are not logged in!")
+    @wps_server = WpsServer.find(params[:id])
+
+    if not User.canAccessObject(current_user, @wps_server)
+      showError("Insufficient permissions -- you cannot access this object!")
       return
     end
 
-    @wps_server = WpsServer.find(params[:id])
-
     respond_to do |format|
       if @wps_server.update_attributes(params[:wps_server])
-        format.html { redirect_to @wps_server, notice: 'Wps server was successfully updated.' }
+        format.html { redirect_to @wps_server, notice: "Wps server was successfully updated." }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -90,16 +94,19 @@ class WpsServersController < ApplicationController
     end
   end
 
+
   # DELETE /wps_servers/1
   # DELETE /wps_servers/1.json
   def destroy
-    if not user_signed_in?    # Should always be true
-      showError("Insufficient permissions -- you are not logged in!")
+    @wps_server = WpsServer.find(params[:id])
+
+    if not User.canAccessObject(current_user, @wps_server)
+      showError("Insufficient permissions -- you cannot access this object!")
       return
     end
-
-    @wps_server = WpsServer.find(params[:id])
-    @wps_server.destroy
+    
+    @wps_server.deleted = true
+    @wps_server.save
 
     respond_to do |format|
       format.html { redirect_to wps_servers_url }
