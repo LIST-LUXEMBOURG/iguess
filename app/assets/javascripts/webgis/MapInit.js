d0165ec31e50f3d24fb02112070962dcff76d3c9
Christopher Eykamp <christopher.eykamp@tudor.lu>
2013-07-25 18:00:00 +0200

    Add missing epsg codes

--- app/assets/javascripts/webgis/MapInit.js
+++ app/assets/javascripts/webgis/MapInit.js
@@ -37,6 +37,7 @@ Proj4js.defs["EPSG:27700"] = "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x
 Proj4js.defs["EPSG:28992"] = "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs";
 Proj4js.defs["EPSG:31467"] = "+proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs";
 
+
 WebGIS.initMap = function () 
 {
 	var mapProjection = new OpenLayers.Projection(WebGIS.mapProjection);
