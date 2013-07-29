3643a7274712119205e3dbcdec8d1e174d86aec2
Luís de Sousa <luis.a.de.sousa@gmail.com>
2013-07-29 09:41:44 +0200

    Added Luxembourgish CRS to Seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
  ['Rotterdam',   'http://ows.gis.rotterdam.nl/cgi-bin/mapserv.exe?map=d:\gwr\webdata\mapserver\map\gwr_basis_pub.map', 12, 'EPSG:28992', 497500,  6786500],
  ['Brussels',    '', 12, 'EPSG:31370', 484517,  6594220],
  ['London',      '', 13, 'EPSG:27700', -8468,  6711661],
- ['Luxembourg',  '', 13, 'EPSG:3857', 682476,  6379117],
- ['Esch-sur-Alzette', '', 14, 'EPSG:3857', 666249,  6359446]
+ ['Luxembourg',  '', 13, 'EPSG:2169', 682574,  6379134],
+ ['Esch-sur-Alzette', '', 14, 'EPSG:2169', 665606,  6359849]
 ].each do |v|
   c = City.find_or_create_by_name v[0]
   c.url =  v[1]
