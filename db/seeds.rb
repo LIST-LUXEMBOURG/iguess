027513a51c6b51582c8d0b2427bb218aa7b0d1d9
Luís de Sousa <luis.a.de.sousa@gmail.com>
2013-07-29 08:47:55 +0200

    Seeds for the LAMILO cities.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,13 @@
 
 [['Aberdeen',    '', 13, 'EPSG:27700', -235500, 7790000],
  ['Gent',        'http://gentgis2.gent.be/arcgisserver/services/G_WIS/testIvago/MapServer/WFSServer', 12, 'EPSG:31370',  415000,  6632500],
- ['Ludwigsburg', 'http://logis.ludwigsburg.de/mapguide2011/mapagent/mapagent.fcgi', 14, 'EPSG:31467', 497500, 6257200]#, 3536658],
+ ['Ludwigsburg', 'http://logis.ludwigsburg.de/mapguide2011/mapagent/mapagent.fcgi', 14, 'EPSG:31467', 497500, 6257200]
  ['Montreuil',   'http://montreuil.dynmap.com/carte_pour_iguess/carteWS.php', 14, 'EPSG:2154',  272000,  6250800],
- ['Rotterdam',   'http://ows.gis.rotterdam.nl/cgi-bin/mapserv.exe?map=d:\gwr\webdata\mapserver\map\gwr_basis_pub.map', 12, 'EPSG:28992', 497500,  6786500]
+ ['Rotterdam',   'http://ows.gis.rotterdam.nl/cgi-bin/mapserv.exe?map=d:\gwr\webdata\mapserver\map\gwr_basis_pub.map', 12, 'EPSG:28992', 497500,  6786500],
+ ['Brussels',    '', 12, 'EPSG:31370', 484517,  6594220],
+ ['London',      '', 13, 'EPSG:27700', -8468,  6711661],
+ ['Luxembourg',  '', 13, 'EPSG:3857', 682476,  6379117],
+ ['Esch-sur-Alzette', '', 14, 'EPSG:3857', 666249,  6359446]
 ].each do |v|
   c = City.find_or_create_by_name v[0]
   c.url =  v[1]
