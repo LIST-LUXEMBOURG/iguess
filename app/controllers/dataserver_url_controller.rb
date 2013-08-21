04f438c9566ac22d89b03190224685b5be18c98d
Christopher Eykamp <christopher.eykamp@tudor.lu>
2013-08-21 18:42:28 +0200

    Prevent duplicate presets from getting into the database... shouldn't have happened, but just to be sure

--- app/controllers/dataserver_url_controller.rb
+++ app/controllers/dataserver_url_controller.rb
@@ -14,11 +14,15 @@ class DataserverUrlController < ApplicationController
    url    = params[:url]
    cityId = params[:city][:id]
 
-   @dataserver_url = DataserverUrl.new
-   @dataserver_url.city_id = cityId
-   @dataserver_url.url     = url
-   @dataserver_url.descr   = ''
-   @dataserver_url.save
+   # Confirm this is not a duplicate...
+   u = DataserverUrl.find_by_city_id_and_url(cityId, url)
+   if not u
+     @dataserver_url = DataserverUrl.new
+     @dataserver_url.city_id = cityId
+     @dataserver_url.url     = url
+     @dataserver_url.descr   = ''
+     @dataserver_url.save
+    end
 
 
     respond_to do |format|
