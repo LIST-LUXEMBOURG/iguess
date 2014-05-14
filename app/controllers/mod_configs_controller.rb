079b218b2d821aebd4445529438db7ae59f86316
Christopher Eykamp <christopher.eykamp@tudor.lu>
2014-05-14 12:27:48 +0200

    Try different error message format

--- app/controllers/mod_configs_controller.rb
+++ app/controllers/mod_configs_controller.rb
@@ -252,6 +252,7 @@ class ModConfigsController < ApplicationController
 
       if error == nil then    # We could not run wpsstart... output IS the error message
         error = "WPSClient Configuration Error: " + output
+        error = re.sub('\n', '<br>\n', error)
       end
 
       # Show error to client
