048011e12e3dfb5b99f9a2ed2cb76f6ee3515ab0
Christopher Eykamp <christopher.eykamp@tudor.lu>
2014-05-22 10:40:54 +0200

    Fix model

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,7 +2,7 @@ class City < ActiveRecord::Base
   default_scope :order => "name"    # Retrieve cities sorted by name
   has_one :dataset
   has_one :mod_config
-  has_many :dataserver_urls
+  has_many :dataserver_url
   has_many :wps_servers
   has_many :wps_processes, through: :wps_servers
   belongs_to :site_detail
