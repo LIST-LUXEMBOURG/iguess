3ce20cb2e35980e7d154dbcb6b20d5a7b5402ba5
Luís de Sousa <luis.a.de.sousa@gmail.com>
2014-06-19 17:42:34 +0200

    Inserting zeros as default values for the Electricity and Heat mixes.

--- app/controllers/co2_scenarios_controller.rb
+++ app/controllers/co2_scenarios_controller.rb
@@ -89,7 +89,7 @@ class Co2ScenariosController < ApplicationController
         m = Co2ElecMix.new
         m.period = period
         m.co2_source = s
-        m.value = period + s.id * 10
+        m.value = 0.0
 
         @elec_mixes[[period, s.id]] = m
       }
@@ -99,7 +99,7 @@ class Co2ScenariosController < ApplicationController
         m = Co2HeatMix.new
         m.period = period
         m.co2_source = s
-        m.value = period + s.id * 10
+        m.value = 0.0
 
         @heat_mixes[[period, s.id]] = m
       }
