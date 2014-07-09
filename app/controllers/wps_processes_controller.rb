60ad46d91ac2c59224a4a5bb756a2db4b19ebdb6
Christopher Eykamp <christopher.eykamp@tudor.lu>
2014-07-09 12:15:26 +0200

    Formatting

--- app/controllers/wps_processes_controller.rb
+++ app/controllers/wps_processes_controller.rb
@@ -3,7 +3,8 @@ class WpsProcessesController < ApplicationController
   # Returns a list of identifiers of registered processes for the specified url 
   # Will only be called via ajax
   def process_query
-    @server = WpsServer.find_by_url_and_city_id(params["server_url"], User.getCurrentCity(current_user, cookies).id)
+    @server = WpsServer.find_by_url_and_city_id(params["server_url"], 
+                                                User.getCurrentCity(current_user, cookies).id)
 
     if @server   
       @processes = @server.wps_processes.map{ |p| p.identifier }
