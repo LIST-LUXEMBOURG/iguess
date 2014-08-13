3d3d9200607890d367755752a9fc080da30160bf
Luís de Sousa <luis.a.de.sousa@gmail.com>
2014-08-13 16:38:33 +0200

    Draft structure for the add/remove period buttons.

--- app/assets/javascripts/co2/AddRemovePeriods.js
+++ app/assets/javascripts/co2/AddRemovePeriods.js
@@ -23,3 +23,51 @@
  
 var CO2 = CO2 || { };		// Create namespace
 
+/***************************************************
+ * This method is still missing the onChange event
+ */
+CO2.addPeriodTable = function(tableName, inputName, newPeriod)
+{
+	var newRow = $('#' + tableName + ' tr:last').clone();
+	newRow.children()[i].firstChild.innerHTML = newPeriod;
+	for(i = 1; i < newRow.children.length; i++)
+	{
+		var input = newRow.children()[i].firstChild;
+		input.setAttribute("value", "0.0");
+		sourceId = input.name.split("[")[1].split("]")[0];
+		input.setAttribute("name", inputName + "[" + newPeriod + "][" + sourceId + "]");
+	}
+};
+
+CO2.addPeriod = function()
+{
+	numPeriods = $('#tableElecMix tr').length;
+	baseYear   = $('co2_scenario[base_year]').val();
+	timeStep   = $('co2_scenario[time_step]').val();
+	newPeriod  = baseYear + timeStep * numPeriods;
+	
+	//Electricity Mix
+	CO2.addPeriodTable("tableElecMix", "co2_elec_mixes", newPeriod);
+	//Heat Mix
+	CO2.addPeriodTable("tableHeatMix", "co2_heat_mixes", newPeriod);
+	//Factors
+	CO2.addPeriodTable("table_co2_factor", "co2_factor", newPeriod);
+	CO2.addPeriodTable("table_ch4_factor", "ch4_factor", newPeriod);
+	CO2.addPeriodTable("table_n2o_factor", "n2o_factor", newPeriod);
+	
+	// Consumptions
+	//"tableCons" + sectorName
+	
+
+};
+
+CO2.removePeriod = function()
+{
+	// Remove last row of a table
+	$('#tableElecMix tr:last').remove();
+	$('#tableHeatMix tr:last').remove();
+	$('#table_co2_factor tr:last').remove();
+	$('#table_ch4_factor tr:last').remove();
+	$('#table_n20_factor tr:last').remove();
+
+};
