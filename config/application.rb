551e2969e0c309868700d24a1a3f20780193a7b2
Luís de Sousa <luis.a.de.sousa@gmail.com>
2014-08-27 15:45:43 +0200

    First solution to Mantis issue #101

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,8 @@ module Iguess
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.assets.paths << Rails.root.join("app", "assets", "documents")
 
     config.before_configuration do
         env_file = File.join(Rails.root, 'config', 'local_env.yml')
