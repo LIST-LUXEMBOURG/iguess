ceadc691bc7fd0bad1cff0d0741f385c6c5a8a9b
Luís de Sousa <luis.a.de.sousa@gmail.com>
2014-09-17 11:05:58 +0200

    Closes #150.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   end
 
   def update
+    
     @user = User.find(params[:id])
 
     if not User.canAccessObject(current_user, @user)
@@ -19,17 +20,34 @@ class UsersController < ApplicationController
     end
 
     respond_to do |format|
+      params[:user][:approved] = 1
       if @user.update_attributes(params[:user], :without_protection => true) then
       	# @user.approved = params[:user][:approved]
       	# @user.save 
 
-        format.html { redirect_to '/', notice: "User was successfully updated." }
+        format.html { redirect_to '/', notice: "User was successfully approved." }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
+  end
+  
+  def reject
+    
+    @user = User.find(params[:id])
+
+    if not User.canAccessObject(current_user, @user)
+      showError("Insufficient permissions -- you cannot modify this object!")
+      return
+    end
+    
+    respond_to do |format|
+      @user.delete
+      format.html { redirect_to '/', notice: "User was successfully rejected." }
+      format.json { head :no_content }
+    end           
   end
 
 end
