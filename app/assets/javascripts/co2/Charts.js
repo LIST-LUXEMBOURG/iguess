dea99e6deb94eb9b5f4b25152bbc276dc6b0c149
Luís de Sousa <luis.a.de.sousa@gmail.com>
2014-11-05 11:48:25 +0100

    Included dummy pie chart.

--- app/assets/javascripts/co2/Charts.js
+++ app/assets/javascripts/co2/Charts.js
@@ -104,3 +104,50 @@ CO2.chartEmissions = function (div, title, units, series)
     });
 };
 
+CO2.pieChart = function()
+{
+    $('#pie_chart').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: 1,//null,
+            plotShadow: false
+        },
+        title: {
+            text: 'Browser market shares at a specific website, 2014'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Browser share',
+            data: [
+                ['Firefox',   45.0],
+                ['IE',       26.8],
+                {
+                    name: 'Chrome',
+                    y: 12.8,
+                    sliced: true,
+                    selected: true
+                },
+                ['Safari',    8.5],
+                ['Opera',     6.2],
+                ['Others',   0.7]
+            ]
+        }]
+    });
+};
+
