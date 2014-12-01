24faf2d8ed151213243635bc85065509397ae3bd
Luís de Sousa <luis.a.de.sousa@gmail.com>
2014-12-01 11:56:39 +0100

    Now tacking care of ticket status and user_id in controller.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,11 +1,18 @@
 class TicketsController < ApplicationController
+  
+  before_filter :authenticate_user!
+  before_filter {|t| t.set_active_tab("tickets") }
+
+  respond_to :html, :json, :js   # See http://railscasts.com/episodes/224-controllers-in-rails-3, c. min 7:00
+
   def new
     @ticket_types = TicketType.all
   end
  
   def create
     @ticket = Ticket.new(params[:ticket])
- 
+    @ticket.ticket_status_id = TicketStatus.find_by_value("Open").id
+    @ticket.user_id = current_user.id
     @ticket.save
     redirect_to @ticket
   end
