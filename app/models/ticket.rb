e22a3dc1055df2827846860b0888feb1d4456ad2
Luís de Sousa <luis.a.de.sousa@gmail.com>
2014-12-02 16:44:07 +0100

    Image input in Ticket new view.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,4 +4,12 @@ class Ticket < ActiveRecord::Base
   belongs_to :ticket_type
   
   has_many   :ticket_follow_ups
+  
+  # Image storage tutorial:
+  # http://archive.railsforum.com/viewtopic.php?id=4642
+  def image_file=(input_data)
+    #self.filename = input_data.original_filename
+    #self.content_type = input_data.content_type.chomp
+    self.image = input_data.read
+  end
 end
