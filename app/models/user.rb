214d12bda81bbbff752e12e06d3e327aad5e5067
Luís de Sousa <luis.a.de.sousa@gmail.com>
2015-01-09 10:11:04 +0100

    User e-mail replaced by name in registration request message.

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ActiveRecord::Base
   attr_accessor :login
 
 
-  def send_admin_mail
+  def send_admin_mail()
     AdminMailer.new_user_waiting_for_approval(self).deliver
   end
 
@@ -90,23 +90,28 @@ end
 
 
 class AdminMailer < ActionMailer::Base
-  default to: Proc.new { "luis.a.de.sousa@gmail.com" }, #"luis.desousa@list.lu" },
+          # It should be possible to include all admins with the "to" directive
+          # but it is not working
+  default to: Proc.new { "iguess@list.lu" },
           from: 'iguess@list.lu'
 
 
   def new_user_waiting_for_approval(user)
     @user = user
-    @url  = 'http://iguess.tudor.lu/users/edit/' + user.id.to_s
+    # The domain model does not effectively link users to a particular site.
+    # Thus the default URL is sent.
+    @url  = 'http://iguess.list.lu/users/edit/' + user.id.to_s
     admins = User.find_all_by_is_admin true
     addr_list = Array.new
     admins.each do |admin| addr_list.push(admin.email) end
-    binding.pry
-    mail(to: addr_list, subject: "New User Awaiting Approval: #{@user.email}")
+    mail(to: addr_list, subject: "New User Awaiting Approval: #{@user.first_name} #{@user.last_name}")
   end
   
   def welcome_email(user)
     @user = user
-    @url  = 'http://iguess.tudor.lu'
+    # The domain model does not effectively link users to a particular site.
+    # Thus the default URL is sent.
+    @url  = 'http://iguess.list.lu'
     mail(to: @user.email, subject: 'Welcome to iGUESS!')
   end
 end
