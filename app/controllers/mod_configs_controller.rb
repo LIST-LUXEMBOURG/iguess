4c4ac6b5c7d12d1b593be4ba0096a9349326573f
Luís de Sousa <luis.a.de.sousa@gmail.com>
2015-03-16 13:51:37 +0100

    Removed forgotten binding.pry.

--- app/controllers/mod_configs_controller.rb
+++ app/controllers/mod_configs_controller.rb
@@ -176,8 +176,6 @@ class ModConfigsController < ApplicationController
     @mod_config.status = ReadyCode
     @mod_config.run_status_id = ReadyCode
     @mod_config.save
-    
-    binding.pry
 
     respond_with do |format|
       format.js { render :json => @mod_config.to_json(:include => :run_status), :status => :ok }
